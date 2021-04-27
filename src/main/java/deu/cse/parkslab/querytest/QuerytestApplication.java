0c14f639c5dc9b91a77529acb13215cea6b23ad0
SWKANG0525 <kanghodong@kakao.com>
2021-04-27 10:51:46 +0900

    Project Settings

---
+++ src/main/java/deu/cse/parkslab/querytest/QuerytestApplication.java
@@ -0,0 +1,13 @@
+package deu.cse.parkslab.querytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerytestApplication.class, args);
+    }
+
+}
